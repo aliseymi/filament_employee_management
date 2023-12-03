c1a48b1c5367a050f4b95f67351029a1b7206c09
ali seymi <seyedali1377mir@gmail.com>
2023-12-03 22:14:39 +0330

    customizing created notification and edit notification

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -16,6 +16,7 @@ use Filament\Forms\Set;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Filters\Filter;
@@ -221,6 +222,12 @@ class EmployeeResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()->successNotification(
+                    Notification::make()
+                    ->success()
+                    ->title('Employee Deleted.')
+                    ->body('The employee deleted successfully.')
+                )
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
